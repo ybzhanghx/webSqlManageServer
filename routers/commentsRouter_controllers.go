ab05ae81707f447d5e888bd5c1c5eca46d14598a
ybzhanghx <751761610@qq.com>
2020-09-10 14:47:16 +0800

    init swagger

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,55 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:DBTBInfoManagerController"] = append(beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:DBTBInfoManagerController"],
+        beego.ControllerComments{
+            Method: "TableConfig",
+            Router: `/TableConfig`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:DBTBInfoManagerController"] = append(beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:DBTBInfoManagerController"],
+        beego.ControllerComments{
+            Method: "GetAllTable",
+            Router: `/getAllTables`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:DBTBInfoManagerController"] = append(beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:DBTBInfoManagerController"],
+        beego.ControllerComments{
+            Method: "UpdateTableConfig",
+            Router: `/updateTableConfig`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:TableDataManagerController"] = append(beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:TableDataManagerController"],
+        beego.ControllerComments{
+            Method: "DataList",
+            Router: `/list`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:TableDataManagerController"] = append(beego.GlobalControllerRouter["bailun.com/CT4_quote_server/WebManageSvr/controllers:TableDataManagerController"],
+        beego.ControllerComments{
+            Method: "UpdateTableData",
+            Router: `/updateTableData`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
